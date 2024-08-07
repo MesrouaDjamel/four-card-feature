067e63baa5224ca899457daebdb772e384e4338c
Mesroua Djamel <mesrouadjamel@gmail.com>
2024-08-07 21:11:20 +0100

    Correction in Container component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = ({children}:{children: React.ReactNode}) => {
   return (
-    <div className="flex flex-col items-center gap-16 mx-[32px] my-[78px] lg:mx-[164px] lg:my-[80px]">
+    <div className="flex flex-col items-center gap-16 mx-[32px] my-[78px] lg:mx-[104px] lg:my-[80px]">
       {children}
     </div>
   );
